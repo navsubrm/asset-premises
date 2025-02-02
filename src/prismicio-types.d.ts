bacca42f43d148c5abc93794fb642aa9a6ee6df0
Nathan M <70189952+navsubrm@users.noreply.github.com>
2025-02-01 21:13:22 -0700

    update commit

--- src/prismicio-types.d.ts
+++ src/prismicio-types.d.ts
@@ -4,7 +4,7 @@ import type * as prismic from '@prismicio/client';
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
-type PageDocumentDataSlicesSlice = RichTextSlice;
+type PageDocumentDataSlicesSlice = BasicHeroSlice | RichTextSlice;
 
 /**
  * Content for Page documents
